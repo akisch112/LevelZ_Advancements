ca987cac187e5b16b4bdc53b93db69ace349d38a
Alex Kisch <kischalex112@gmail.com>
2025-10-07 00:41:30 -0400

    bootable version

---
+++ src/main/java/com/alexkisch/levelz/LevelZAdvancements.java
@@ -0,0 +1,12 @@
+package com.alexkisch.levelz;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LevelZAdvancements implements ModInitializer {
+    public static final String MOD_ID = "levelz_advancements";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("✅ LevelZ Advancements mod initialized!");
+    }
+}
---
+++ src/main/java/com/alexkisch/levelz/LevelZAdvancementsClient.java
@@ -0,0 +1,10 @@
+package com.alexkisch.levelz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LevelZAdvancementsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("🎮 LevelZ Advancements client initialized!");
+    }
+}
